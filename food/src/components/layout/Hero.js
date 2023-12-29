76596c00fdf6dc385d1689cb2318cc722a763504
nallaperumal007 <nallaperumal342@gmail.com>
2023-12-29 09:24:14 +0530

    Header component is finish

---
+++ food/src/components/layout/Hero.js
@@ -0,0 +1,13 @@
+
+export default function Hero() {
+  return (
+    <>
+    <section>
+        <h1>Delicious Signature Pizzas Await You!</h1>
+        <p>Explore a taste adventure with our crafted pizzas,Love and care in every pizza,
+         for your delight.Savor freshness in every delicious bite.Experience a masterpiece in 
+         every slice.Elevate your dining joy with us</p>
+    </section>
+    </>
+  )
+}
